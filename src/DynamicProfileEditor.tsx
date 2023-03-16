4d37bc60c9de33c8d4e6074583f8a3c513934c2c
solid-akb <anna.kallenbornbolden@inrupt.com>
2023-03-16 14:21:59 -0700

    wip

--- src/DynamicProfileEditor.tsx
+++ src/DynamicProfileEditor.tsx
@@ -1,8 +1,5 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import invariant from "tiny-invariant";
-import MenuItem from "@mui/material/MenuItem";
 import React, { memo, useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Divider, Link, Typography } from "@mui/material";
@@ -10,8 +7,13 @@ import NewProfileView from "./NewProfileView";
 import { useNavigate } from "react-router-dom";
 import { useSession } from "@inrupt/solid-ui-react";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
-import useProfile from "./hooks/useProfile";
-import { getSolidDataset, SolidDataset, Thing } from "@inrupt/solid-client";
+import {
+  getSolidDataset,
+  getThing,
+  getThingAll,
+  SolidDataset,
+  Thing,
+} from "@inrupt/solid-client";
 import { greedyGetDataFromThing } from "./utils/greedyGetDataFromThing";
 
 const DynamicProfileEditor = () => {
@@ -20,41 +22,51 @@ const DynamicProfileEditor = () => {
   const [profileDataset, setProfileDataset] = useState<
     SolidDataset | undefined
   >();
-  const [fields, setFields] = useState<Thing[]>([]);
+  const [profileThings, setProfileThings] = useState<Thing[]>([]);
   const [showSuccess, setShowSuccess] = useState(false);
 
   const handleGoBackToProfileSelector = () => {
     // remove selected profile
     navigate("/profile-selector");
   };
+
+  const handleSave = () => {
+    console.log("save profile");
+  };
+
   const selectedProfileUrl =
     "https://storage.inrupt.com/d0f9cb3c-2187-4363-86f2-30944951f5ec/profile";
 
   const getProfileData = async () => {
-    const dataset = await getSolidDataset(
-      "https://storage.inrupt.com/d0f9cb3c-2187-4363-86f2-30944951f5ec/profile",
-      { fetch: session.fetch }
-    );
+    const dataset = await getSolidDataset(selectedProfileUrl, {
+      fetch: session.fetch,
+    });
+    console.log({ dataset });
+    if (session.info.webId) {
+      const things = await getThingAll(dataset);
+      console.log({ things });
+      setProfileThings(things);
+    }
     setProfileDataset(dataset);
   };
 
   useEffect(() => {
     if (session.info.isLoggedIn) {
       getProfileData();
-      console.log("in useEffect", { profileDataset });
     }
   }, [session]);
 
   const renderProfileForm = () => {
-    return fields.map((field, i) => {
+    return profileThings.map((thing, i) => {
       // we need to use this function because we don't know what type of data each field is
+      console.log({ thing });
       const label =
         greedyGetDataFromThing(
-          field,
+          thing,
           "http://www.w3.org/2000/01/rdf-schema#label"
-        ) || field.toString();
+        )?.toString() || thing.toString();
       return (
-        <Grid2>
+        <Grid2 key={i}>
           <TextField
             variant="filled"
             //   defaultValue={}
@@ -87,7 +99,7 @@ const DynamicProfileEditor = () => {
           <Button
             size="large"
             fullWidth
-            type="submit"
+            onClick={handleSave}
             color="secondary"
             variant="contained"
           >
--- src/utils/greedyGetDataFromThing.ts
+++ src/utils/greedyGetDataFromThing.ts
@@ -1,7 +1,20 @@
-import { getBoolean, getDate, getDatetime, Thing } from "@inrupt/solid-client";
+import {
+  getBoolean,
+  getDate,
+  getDatetime,
+  getDecimal,
+  getInteger,
+  getStringNoLocale,
+  Thing,
+} from "@inrupt/solid-client";
 
 export const greedyGetDataFromThing = (thing: Thing, property: string) => {
+  if (getStringNoLocale(thing, property))
+    return getStringNoLocale(thing, property);
   if (getBoolean(thing, property)) return getBoolean(thing, property);
   if (getDate(thing, property)) return getDate(thing, property);
-  if (getDatetime(thing, property)) return getDateTime(thing, property);
+  if (getDatetime(thing, property)) return getDatetime(thing, property);
+  if (getDecimal(thing, property)) return getDecimal(thing, property);
+  if (getInteger(thing, property)) return getInteger(thing, property);
+  return null;
 };
